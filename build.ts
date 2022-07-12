982f6bc7cb41b91f1a54f5df66b94a89d835568f
José Leonado Toro <86394659+xosedev@users.noreply.github.com>
2022-07-12 18:32:58 -0400

    feature/add ssl to server

--- build.ts
+++ build.ts
@@ -7,5 +7,6 @@ s.mkdir(outDir);
 s.cp('.env', `${outDir}/.env`);
 s.mkdir('-p', `${outDir}/common/swagger`);
 s.cp('src/common/api.yml', `${outDir}/common/api.yml`);
+s.mkdir('-p', `${outDir}/keys`);
 s.cp('src/keys/fullchain.pem', `${outDir}/keys/fullchain.pem`)
 s.cp('src/keys/privkey.pem', `${outDir}/keys/privkey.pem`)
